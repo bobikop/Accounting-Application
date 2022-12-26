a7ab69c74f51d0bdd13ca30c2d3fe39f465a94dc
owermaiden <ower.maiden@cybertekschool.com>
2022-12-26 19:29:56 +0100

    Securoity is ready... Come on Team, it is your turn...

--- src/main/java/com/thegogetters/accounting/repository/ProductRepository.java
+++ src/main/java/com/thegogetters/accounting/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.thegogetters.accounting.repository;
 
+import com.thegogetters.accounting.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository{
+public interface ProductRepository extends JpaRepository<Product, Long>{
 }
