bdda7593d00bb379d90a1cc95e37b6ab6fd5ae33
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-12-26 18:22:25 +0100

    JDZH7-124 - InvoiceProductDTO is updated

---
+++ src/main/java/com/thegogetters/accounting/controller/InvoiceController.java
@@ -0,0 +1,21 @@
+package com.thegogetters.accounting.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class InvoiceController {
+
+    //GetMapping - listInvoices()
+
+    //GetMapping - createInvoice()
+    //PostMapping - createInvoice()
+
+    //GetMapping - editInvoice()
+    //PostMapping - updateInvoice()
+
+    //GetMapping - deleteInvoice()
+
+}
