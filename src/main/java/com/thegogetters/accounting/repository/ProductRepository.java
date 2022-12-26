23bd816fa0103cd84306cb12019d94e11c3c6647
Evgenia0707 <4030071@gmail.com>
2022-12-26 13:25:59 -0500

    Product added

---
+++ src/main/java/com/thegogetters/accounting/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.thegogetters.accounting.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository{
+}
---
+++ src/main/java/com/thegogetters/accounting/service/serviceImpl/ProductServiceImpl.java
@@ -0,0 +1,21 @@
+package com.thegogetters.accounting.service.serviceImpl;
+
+import com.thegogetters.accounting.dto.ProductDTO;
+import com.thegogetters.accounting.service.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public void save(ProductDTO dto) {
+
+    }
+
+    @Override
+    public void update(ProductDTO dto) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
