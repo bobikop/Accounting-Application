bd2620ea0a9043606d5c927723a95d9cb24fc73e
Ermek Apazov <apazovermek@gmail.com>
2022-12-26 12:44:44 -0500

    JDZH7-81 converter is created

---
+++ src/main/java/com/thegogetters/accounting/converter/ClientVendorDtoConverter.java
@@ -0,0 +1,12 @@
+package com.thegogetters.accounting.converter;
+
+import com.thegogetters.accounting.dto.ClientVendorDto;
+import org.springframework.core.convert.converter.Converter;
+
+public class ClientVendorDtoConverter implements Converter<String, ClientVendorDto> {
+
+    @Override
+    public ClientVendorDto convert(String source) {
+        return null;
+    }
+}
