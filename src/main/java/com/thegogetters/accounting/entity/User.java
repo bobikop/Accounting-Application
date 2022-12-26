cd2a00490337d7dfcc03ab8feca28bea0eeecf40
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-12-26 10:30:10 -0500

    Accounting App User Entity

---
+++ src/main/java/com/thegogetters/accounting/entity/User.java
@@ -0,0 +1,32 @@
+package com.thegogetters.accounting.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+@Where(clause = "is_deleted=false")
+public class User extends BaseEntity {
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private boolean enabled;
+//    @ManyToOne
+//    private Role role;
+//    @ManyToOne
+//    private Company company;
+
+
+}
