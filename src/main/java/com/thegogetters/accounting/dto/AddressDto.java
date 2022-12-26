2a76345a6875c5d763ffadba0a060179460fc9c6
owermaiden <ower.maiden@cybertekschool.com>
2022-12-26 16:23:57 +0100

    Address added

--- src/main/java/com/thegogetters/accounting/dto/AddressDto.java
+++ src/main/java/com/thegogetters/accounting/dto/AddressDto.java
@@ -1,4 +1,18 @@
 package com.thegogetters.accounting.dto;
 
 public class AddressDto {
+
+    private Long id;
+
+    private String addressLine1;
+
+    private String addressLine2;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private String zipCode;
 }
