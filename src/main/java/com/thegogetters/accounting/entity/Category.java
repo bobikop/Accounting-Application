ce350295717f4e5dbf4157287026cf6252cb5406
Dilbar <dibux9@gmail.com>
2022-12-26 10:24:28 -0500

    category entity updated

--- src/main/java/com/thegogetters/accounting/entity/Category.java
+++ src/main/java/com/thegogetters/accounting/entity/Category.java
@@ -1,9 +1,18 @@
 package com.thegogetters.accounting.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "categories")
 public class Category extends BaseEntity{
