f56f2deb44097cedcc8bb9fc07bcfe16d083cc90
owermaiden <ower.maiden@cybertekschool.com>
2022-12-26 19:14:43 +0100

    Securoity is ready... Come on Team, it is your turn...

---
+++ src/main/java/com/thegogetters/accounting/service/SecurityService.java
@@ -0,0 +1,9 @@
+package com.thegogetters.accounting.service;
+
+import com.thegogetters.accounting.dto.UserDTO;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface SecurityService extends UserDetailsService {
+
+
+}
