9cc0041a59102ba0c241f07b3528716410bce0f5
Korshad2012 <gihtilali@gmail.com>
2022-12-26 15:01:43 -0500

    JDZH7 - CompanyService Implementation Created

---
+++ src/main/java/com/thegogetters/accounting/service/Impl/CompanyServiceImpl.java
@@ -0,0 +1,17 @@
+package com.thegogetters.accounting.service.impl;
+
+import com.thegogetters.accounting.dto.CompanyDto;
+import com.thegogetters.accounting.service.CompanyService;
+
+import java.util.List;
+
+public class CompanyServiceImpl implements CompanyService {
+
+
+    @Override
+    public List<CompanyDto> listAll() {
+        return null;
+    }
+
+
+}
