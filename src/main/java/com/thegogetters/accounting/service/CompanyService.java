a222301513aa93fecb6e351aebebfb07b49ccf3f
Korshad2012 <gihtilali@gmail.com>
2022-12-26 15:00:14 -0500

    JDZH7 - CompanyService Interface Created

---
+++ src/main/java/com/thegogetters/accounting/service/CompanyService.java
@@ -0,0 +1,20 @@
+package com.thegogetters.accounting.service;
+
+import com.thegogetters.accounting.dto.CompanyDto;
+
+import java.util.List;
+
+public interface CompanyService {
+
+    List<CompanyDto> listAll();
+
+//    CompanyDto findByCompanyId(Long id);
+//
+//    CompanyDto update(CompanyDto companyDto);
+//
+//    void save(CompanyDto companyDto);
+
+
+
+
+}
