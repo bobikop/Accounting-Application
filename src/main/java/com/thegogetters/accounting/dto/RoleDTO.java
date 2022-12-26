6386a3ad012555f30849c096ba3d56c37967b834
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-12-26 11:25:30 -0500

    Accounting App- RoleDTO Entity

---
+++ src/main/java/com/thegogetters/accounting/dto/RoleDTO.java
@@ -0,0 +1,12 @@
+package com.thegogetters.accounting.dto;
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDTO {
+    private Long id;
+    private String description;
+}
