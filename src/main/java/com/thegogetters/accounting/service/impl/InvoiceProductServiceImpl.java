eb589b9d0e015f8335236d1163c5ee94099dc866
owermaiden <ower.maiden@cybertekschool.com>
2022-12-27 13:06:30 +0100

    bug fix

--- src/main/java/com/thegogetters/accounting/service/impl/InvoiceProductServiceImpl.java
+++ src/main/java/com/thegogetters/accounting/service/impl/InvoiceProductServiceImpl.java
@@ -1,6 +1,10 @@
 package com.thegogetters.accounting.service.Impl;
 
 import com.thegogetters.accounting.service.InvoiceProductService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class InvoiceProductServiceImpl implements InvoiceProductService {
+
+    // Hasan
 }
--- src/main/java/com/thegogetters/accounting/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/thegogetters/accounting/service/impl/InvoiceServiceImpl.java
@@ -1,6 +1,10 @@
 package com.thegogetters.accounting.service.Impl;
 
 import com.thegogetters.accounting.service.InvoiceService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class InvoiceServiceImpl implements InvoiceService {
+
+    //Hasan
 }
