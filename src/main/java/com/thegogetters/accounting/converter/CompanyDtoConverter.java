c87b3c5e4fea8df169b4070ccffebc89de394c24
Korshad2012 <gihtilali@gmail.com>
2022-12-26 23:18:27 -0500

    JDZH7 - CompanyDtoConverter class created

--- src/main/java/com/thegogetters/accounting/converter/CompanyDtoConverter.java
+++ src/main/java/com/thegogetters/accounting/converter/CompanyDtoConverter.java
@@ -1,11 +1,22 @@
 package com.thegogetters.accounting.converter;
 
 import com.thegogetters.accounting.dto.CompanyDto;
+import com.thegogetters.accounting.service.CompanyService;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CompanyDtoConverter implements Converter<String, CompanyDto> {
+
+    private final CompanyService companyService;
+
+    public CompanyDtoConverter(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
     @Override
     public CompanyDto convert(String source) {
-        return null;
+
+        return companyService.findById(Long.parseLong(source));
     }
 }
