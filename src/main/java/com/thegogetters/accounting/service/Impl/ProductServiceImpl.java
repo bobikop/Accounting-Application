003094b1c9f1919c421a73332aea13b9694795ca
Evgenia0707 <4030071@gmail.com>
2022-12-27 11:19:03 -0500

    Remove ProductMapper

--- src/main/java/com/thegogetters/accounting/service/Impl/ProductServiceImpl.java
+++ src/main/java/com/thegogetters/accounting/service/Impl/ProductServiceImpl.java
@@ -2,50 +2,44 @@ package com.thegogetters.accounting.service.Impl;
 
 import com.thegogetters.accounting.dto.ProductDTO;
 import com.thegogetters.accounting.entity.Product;
-import com.thegogetters.accounting.mapper.ProductMapper;
+import com.thegogetters.accounting.mapper.MapperUtil;
 import com.thegogetters.accounting.repository.ProductRepository;
 import com.thegogetters.accounting.service.ProductService;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.stream.Collectors;
-
 @Service
 public class ProductServiceImpl implements ProductService {
-
-    private final ProductMapper productMapper;
-
+    private final MapperUtil mapperUtil;
     private final ProductRepository productRepository;
 
-    public ProductServiceImpl(ProductMapper productMapper, ProductRepository productRepository) {
-        this.productMapper = productMapper;
+    public ProductServiceImpl(MapperUtil mapperUtil, ProductRepository productRepository) {
+        this.mapperUtil = mapperUtil;
         this.productRepository = productRepository;
     }
-
-    @Override
-    public ProductDTO getProductById(Long id) {
-        Product product = productRepository.findProductById(id);
-        return productMapper.convertToDto(product);
-    }
-
     @Override
     public List<ProductDTO> listAllProducts() {
         return productRepository.findAll().stream()
-                .map(productMapper::convertToDto)
+                .map(product -> mapperUtil.convert(product, new ProductDTO()))
                 .collect(Collectors.toList());
     }
-
+    @Override
+    public ProductDTO getProductById(Long id) {
+        Product product = productRepository.findProductById(id);
+        return mapperUtil.convert(product, new ProductDTO());
+    }
     @Override
     public void save(ProductDTO dto) {
-    //TODO
+        Product product = mapperUtil.convert(dto, new Product());
+        productRepository.save(product);
     }
-
     @Override
     public void update(ProductDTO dto) {
-    //TODO
+        //TODO
     }
-
     @Override
     public void deleteById(Long id) {
-    //TODO
+        //TODO
     }
 }
