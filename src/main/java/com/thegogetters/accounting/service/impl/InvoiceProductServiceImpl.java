c6f38fba4a3adadc34413426633f2e0f1d292137
Omer Erden <93406612+owermaiden@users.noreply.github.com>
2022-12-27 13:05:08 +0100

    Delete InvoiceProductServiceImpl.java

--- src/main/java/com/thegogetters/accounting/service/impl/InvoiceProductServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.thegogetters.accounting.service.Impl;
-
-import com.thegogetters.accounting.service.InvoiceProductService;
-
-public class InvoiceProductServiceImpl implements InvoiceProductService {
-}
