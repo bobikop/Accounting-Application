7fdb565601260618197e39ebd189a55b33bc5cd3
Korshad2012 <gihtilali@gmail.com>
2022-12-28 08:47:35 -0500

    DashboardService interface and service impl class created

--- src/main/java/com/thegogetters/accounting/controller/DashboardController.java
+++ src/main/java/com/thegogetters/accounting/controller/DashboardController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DashboardController {
 
 
-    @GetMapping
+    @GetMapping()
     public String retrieveDashboard(){
 
         return "/dashboard";
