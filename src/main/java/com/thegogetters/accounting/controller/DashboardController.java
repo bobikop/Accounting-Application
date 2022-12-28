de5fd3eb84ad4929620b05f02913cdd0a4e0612a
Korshad2012 <gihtilali@gmail.com>
2022-12-28 08:20:58 -0500

    DashboardController class created retreiveDashboard method added UserDto @Getter and @Setter method added UserServiceImpl implemented UserService Interface

---
+++ src/main/java/com/thegogetters/accounting/controller/DashboardController.java
@@ -0,0 +1,17 @@
+package com.thegogetters.accounting.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+
+    @GetMapping
+    public String retrieveDashboard(){
+
+        return "/dashboard";
+    }
+}
