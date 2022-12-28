5009298a4e6d546dd9ff2ba07533f43189f608b1
Korshad2012 <gihtilali@gmail.com>
2022-12-28 15:34:19 -0500

    DashboardController completed

--- src/main/java/com/thegogetters/accounting/controller/DashboardController.java
+++ src/main/java/com/thegogetters/accounting/controller/DashboardController.java
@@ -1,16 +1,44 @@
 package com.thegogetters.accounting.controller;
 
+import com.thegogetters.accounting.dto.ClientVendorDto;
+import com.thegogetters.accounting.dto.ExchangeRate;
+import com.thegogetters.accounting.dto.InvoiceDTO;
+import com.thegogetters.accounting.enums.InvoiceStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 
 
-    @GetMapping()
-    public String retrieveDashboard(){
+    @GetMapping
+    public String retrieveDashboard(Model model){
+
+        Map<String, Double> summaryNumbers = new HashMap<String, Double>();
+
+        summaryNumbers.put("totalCost",1000.00);
+        summaryNumbers.put("totalSales",1000.00);
+        summaryNumbers.put("profitLoss",1000.00);
+
+        InvoiceDTO invoiceDTO = new InvoiceDTO("001", LocalDate.now(),new ClientVendorDto(),new BigDecimal(600),60,new BigDecimal(660));
+
+        List<InvoiceDTO> invoiceDTOS = new ArrayList<>();
+        invoiceDTOS.add(invoiceDTO);
+
+        model.addAttribute("summaryNumbers", summaryNumbers);
+        model.addAttribute("invoices",invoiceDTOS);
+        model.addAttribute("exchangeRates", new ExchangeRate());
+
 
         return "/dashboard";
     }
