42d9a903d23665e12a874dc12375185cb9466e16
Korshad2012 <gihtilali@gmail.com>
2022-12-28 08:24:08 -0500

    DashboardService interface and service impl class created

--- src/main/java/com/thegogetters/accounting/service/Impl/UserServiceImpl.java
+++ src/main/java/com/thegogetters/accounting/service/Impl/UserServiceImpl.java
@@ -1,7 +1,41 @@
 package com.thegogetters.accounting.service.Impl;
 
 
+import com.thegogetters.accounting.dto.UserDTO;
+import com.thegogetters.accounting.service.UserService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDTO findByUserName(String username) {
+        return null;
+    }
+
+    @Override
+    public List<UserDTO> listAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void save(UserDTO user) {
+
+    }
+
+    @Override
+    public void deleteByUserName(String username) {
+
+    }
+
+    @Override
+    public UserDTO update(UserDTO user) {
+        return null;
+    }
+
+    @Override
+    public List<UserDTO> listAllByRole(String role) {
+        return null;
+    }
 }
