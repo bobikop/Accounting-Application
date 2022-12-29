28a6898f425f009784b3a928cff3835a1414abf5
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-12-28 19:35:51 -0500

    Accounting App - Create UserController methods, UserService and RoleService Implementation

--- src/main/java/com/thegogetters/accounting/service/Impl/UserServiceImpl.java
+++ src/main/java/com/thegogetters/accounting/service/Impl/UserServiceImpl.java
@@ -6,20 +6,30 @@ import com.thegogetters.accounting.entity.User;
 import com.thegogetters.accounting.mapper.MapperUtil;
 import com.thegogetters.accounting.repository.UserRepository;
 import com.thegogetters.accounting.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    //DI Services
     private final UserRepository userRepository;
     private final MapperUtil mapperUtil;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, MapperUtil mapperUtil) {
+
+    public UserServiceImpl(UserRepository userRepository, MapperUtil mapperUtil, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.mapperUtil = mapperUtil;
+        this.passwordEncoder = passwordEncoder;
     }
+
+
+    // Method implementation
 
     @Override
     public List<UserDTO> listAllUsers() {
@@ -29,14 +39,19 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void save(UserDTO user) {
-
-
+    public void save(UserDTO userDTO) {
+        User user1 = mapperUtil.convert(userDTO, new User());
+        user1.setPassword(passwordEncoder.encode(user1.getPassword()));
+        userRepository.save(user1);
     }
 
     @Override
-    public UserDTO update(UserDTO user) {
-        return null;
+    public void update(UserDTO userDTO) {
+        User user1 = userRepository.findById(userDTO.getId()).get();
+        User convertedUser = mapperUtil.convert(userDTO,new User());
+        convertedUser.setId(user1.getId());
+        userRepository.save(convertedUser);
+
     }
 
     @Override
@@ -49,13 +64,24 @@ public class UserServiceImpl implements UserService {
     public void deleteUser(String username) {
 
         User user = userRepository.findByUsername(username);
-
         user.setIsDeleted(true);
         user.setUsername(user.getUsername() + "-" + user.getId());
-
         userRepository.save(user);
 
     }
+
+    @Override
+    public UserDTO findById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(()->new NoSuchElementException("User not found"));
+        return mapperUtil.convert(user, new UserDTO());
+    }
+
+    @Override
+    public List<UserDTO> getAllUsersByLoggedInUser() {
+        return null;
+    }
+
+
 
 
 }
--- src/main/java/com/thegogetters/accounting/service/UserService.java
+++ src/main/java/com/thegogetters/accounting/service/UserService.java
@@ -7,8 +7,22 @@ import java.util.List;
 public interface UserService {
 
     List<UserDTO> listAllUsers();
-    void save(UserDTO user);
-    UserDTO update(UserDTO user);
+    void save(UserDTO userDTO);
+    void update(UserDTO userDTO);
     UserDTO findByUserName(String username);
     void deleteUser(String username);
+    UserDTO findById(Long id);
+
+
+/*
+    Need to separate listed users depending on who is looged in in to application
+            Case 1: root user is looged in = show all the companies -users -Admins only
+            Case 2: admin is logged in = show managers, admins, employees
+
+*/
+
+
+    List<UserDTO> getAllUsersByLoggedInUser();
+
+
 }
