607f86800dd2ea1cd0639e3409085356ff22a629
Korshad2012 <gihtilali@gmail.com>
2022-12-29 13:46:50 -0500

    ExchangeRate API consumed

--- src/main/java/com/thegogetters/accounting/service/Impl/DashboardServiceImpl.java
+++ src/main/java/com/thegogetters/accounting/service/Impl/DashboardServiceImpl.java
@@ -1,6 +1,42 @@
 package com.thegogetters.accounting.service.impl;
 
+import com.thegogetters.accounting.client.ExchangeClient;
+import com.thegogetters.accounting.dto.ExchangeRate;
 import com.thegogetters.accounting.service.DashboardService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DashboardServiceImpl implements DashboardService {
+
+    private final ExchangeClient exchangeClient;
+
+    public DashboardServiceImpl(ExchangeClient exchangeClient) {
+        this.exchangeClient = exchangeClient;
+    }
+
+
+    @Override
+    public ExchangeRate listUsdExchangeRate() {
+
+        ExchangeRate exchangeRate = new ExchangeRate();
+
+        double euro = exchangeClient.getUsdExchangeRate().getUsd().getEur();
+        exchangeRate.setEuro(euro);
+
+        double gdb = exchangeClient.getUsdExchangeRate().getUsd().getGbp();
+        exchangeRate.setBritishPound(gdb);
+
+        double cad = exchangeClient.getUsdExchangeRate().getUsd().getCad();
+        exchangeRate.setCanadianDollar(cad);
+
+        double jpy = exchangeClient.getUsdExchangeRate().getUsd().getJpy();
+        exchangeRate.setJapaneseYen(jpy);
+
+        double inr = exchangeClient.getUsdExchangeRate().getUsd().getInr();
+        exchangeRate.setIndianRupee(inr);
+
+
+        return exchangeRate;
+
+    }
 }
