37536d0e71a544648c64adc3d543fafc8efd29e5
HasanCanAyhan <ayhanhasancan55@gmail.com>
2023-01-02 16:54:00 +0100

    JDZH7-130/132/134/135/136/138/140/142
    added validation on invoicedto - invoiceproductdto- and message properties

--- src/main/java/com/thegogetters/accounting/dto/InvoiceProductDTO.java
+++ src/main/java/com/thegogetters/accounting/dto/InvoiceProductDTO.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Getter
@@ -14,15 +17,29 @@ import java.math.BigDecimal;
 public class InvoiceProductDTO {
 
     private Long id;
+
+    @NotNull
+    @Max(100)
     private Integer quantity;       // If this is a purchase invoice, you can keep track of how many product you bought
+
+    @NotNull
+    @Min(1)
     private BigDecimal price;       // and how much you spend for that... Price is also here..
+
+    @NotNull
+    @Max(20)
+    @Min(5)
     private Integer tax;            // and tax
+
+
     private BigDecimal total;       // Total Price with tax included
     private BigDecimal profitLoss;  // You need to calculate profit/loss once invoice approved
     private Integer remainingQty;   // Once you approve a purchase invoice, this invoiceProduct remaining_quantity
                                     // will help you to calculate how many item you left from that invoice_product
                                     // Use this value to keep track of your products FIFO logic..
     private InvoiceDTO invoiceDto;
+
+    @NotNull
     private ProductDTO product;
 
 }
