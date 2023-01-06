80c7b9ae8eca9a2acba478475f5dae7c9657648d
Evgenia0707 <4030071@gmail.com>
2023-01-06 18:01:59 -0500

    ProductDTO, ProductRepository, ProductService, ProductServiceImpl refactoring.

--- src/main/java/com/thegogetters/accounting/service/ProductService.java
+++ src/main/java/com/thegogetters/accounting/service/ProductService.java
@@ -1,7 +1,11 @@
 package com.thegogetters.accounting.service;
 
 import com.thegogetters.accounting.custom.exception.AccountingAppException;
+import com.thegogetters.accounting.dto.InvoiceProductDTO;
 import com.thegogetters.accounting.dto.ProductDTO;
+import com.thegogetters.accounting.entity.InvoiceProduct;
+import com.thegogetters.accounting.enums.InvoiceType;
+
 import java.util.List;
 
 public interface ProductService {
@@ -11,8 +15,9 @@ public interface ProductService {
     void update(ProductDTO productDTO) throws AccountingAppException;
     void deleteById(Long id) throws AccountingAppException;
     boolean checkAnyProductExist(Long id);
-//    boolean isInStock(Long id); //handled in html currently
+    boolean isInStockEnough(InvoiceProductDTO invoiceProductDTO);
     List<ProductDTO> getAllProductsByCompany();
     boolean checkAnyInvoiceExist(Long id);
-    boolean isNameExist(String name, Long id) throws AccountingAppException;
+    boolean isNameExist(String name, Long id);
+    void updateProductQuantity(InvoiceType invoiceType, InvoiceProduct invoiceProduct);
 }
