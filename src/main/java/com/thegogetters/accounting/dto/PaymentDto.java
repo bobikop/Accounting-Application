100d09524ef1a9780a11580bbc46e2682d4d7d87
Evgenia0707 <4030071@gmail.com>
2023-01-15 20:12:39 -0500

    Add ProductServiceImplUT.

--- src/main/java/com/thegogetters/accounting/dto/PaymentDto.java
+++ src/main/java/com/thegogetters/accounting/dto/PaymentDto.java
@@ -1,10 +1,7 @@
 package com.thegogetters.accounting.dto;
 
 import com.thegogetters.accounting.enums.Months;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -12,6 +9,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class PaymentDto {
     private Long id;
     private CompanyDto company;
--- src/main/java/com/thegogetters/accounting/dto/RoleDTO.java
+++ src/main/java/com/thegogetters/accounting/dto/RoleDTO.java
@@ -6,6 +6,7 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class RoleDTO {
     private Long id;
     private String description;
